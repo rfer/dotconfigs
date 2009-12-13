18c56d3b7a75b2c3c4119064f8df3f47e55e76e6
Ricardo Ferreira <ricardo@dazzledminds.net>
2009-12-13 16:03:29 +0000

    * Chromium and Pidgin now have their own workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -7,19 +7,25 @@ import XMonad.ManageHook
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.NoBorders
 
+import qualified XMonad.StackSet as W
+import Control.Monad (liftM2)
+
 myManageHook :: [ManageHook]
-myManageHook = [ resource  =? "Do"   --> doIgnore ]
+myManageHook = [ resource  =? "Do"   --> doIgnore,
+		 className =? "Pidgin" --> viewShift "9",
+		 className =? "Chromium-browser" --> viewShift "8"
+		]
+		where viewShift = doF . liftM2 (.) W.greedyView W.shift
 
 main = do 
-xmonad $ gnomeConfig { 
-	modMask = mod4Mask,
-	layoutHook = smartBorders . avoidStruts  $  layoutHook gnomeConfig,
-	manageHook = manageHook gnomeConfig <+> composeAll myManageHook
-} `additionalKeysP`
-
-        [ ("M-<L>", prevWS )
-        , ("M-<R>", nextWS )
-        , ("M-S-<L>", shiftToPrev)
-        , ("M-S-<R>", shiftToNext)
-        ]
+	xmonad $ gnomeConfig { 
+		modMask = mod4Mask,
+		layoutHook = smartBorders . avoidStruts  $  layoutHook gnomeConfig,
+		manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+	} `additionalKeysP`
+		[ ("M-<L>", prevWS )
+		, ("M-<R>", nextWS )
+		, ("M-S-<L>", shiftToPrev)
+		, ("M-S-<R>", shiftToNext)
+		]
 
